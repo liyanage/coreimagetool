074fcde97b2d969f8aacac5b0a618c7da2647522
Marc Liyanage <reg.gitcommit@entropy.ch>
2009-11-09 21:01:26 -0800

    copyright update

--- src/ActionStore.m
+++ src/ActionStore.m
@@ -3,7 +3,7 @@
 //  CoreImageTool
 //
 //  Created by Marc Liyanage on 03.08.07.
-//  Copyright 2007 Marc Liyanage <http://www.entropy.ch>. All rights reserved.
+//  Copyright 2007-2009 Marc Liyanage <http://www.entropy.ch>. All rights reserved.
 //
 
 #import "ActionStore.h"
